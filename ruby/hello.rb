c3c3c6f2b26a14a691082f0c27b0462dc6a70e53
Daniel Silén <daniel@silen.se>
2016-10-23 18:43:36 +0200

    Adding a hello.rb

---
+++ ruby/hello.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#$Id$
+#irb starta interaktiv session
+#eller ruby fil.rb
+puts 'hello, world'
+languague = 'Ruby'
+puts "hello, #{languague}"
+
+languague = 'my Ruby'
+puts "hello, #{languague}"
