50aa0de40bf183dbd8366a89e415125dbe48cf2a
Daniel Silén <daniel@silen.se>
2017-01-21 15:15:58 +0100

    Scala day1

--- scala/day1.scala
+++ scala/day1.scala
@@ -50,11 +50,37 @@ val p = new Person("Nisse","Tuta")
 println(p.nisse)
 Person.aSingleton()
 
-
+// Dagens uppgift på riktigt
 
 class TicTac {
     val e = ""
     val x = "X"
     val y = "Y"
     var board = (e,e,e,e,e,e,e,e,e)
+
+    var (r1c1,r1c2,r1c3,r2c1,r2c2,r2c3,r3c1,r3c2,r3c3) = board
+
+    var row1 = (r1c1,r1c2,r1c3)
+    var row2 = (r2c1,r2c2,r2c3)
+    var row3 = (r3c1,r3c2,r3c3)
+    var col1 = (r1c1,r2c1,r3c1)
+    var col2 = (r1c2,r2c2,r3c2)
+    var col3 = (r1c3,r2c3,r3c3)
+    var diag1 = (r1c1,r2c2,r3c3)
+    var diag2 = (r3c1,r2c2,r1c3)
+
+    var allCombos = List(row1,row2,row3,col1,col2,col3,diag1,diag2)
+
+    def weHaveAWinner:Boolean = {
+        // kolla om nån av sakerna i allCombos är alla samma
+        def allSame(x:String,y:String,z:String):Boolean = 
+            { return x == y && y == z }
+
+        for(i <- allCombos) {
+            var (x,y,z) = i
+            if(allSame(x,y,z))
+                return true
+        }
+        return false
+    }
 }
