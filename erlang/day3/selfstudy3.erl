99ca28e091d4459585dc09e6cf29277deb87bfbf
Daniel Silén <daniel@silen.se>
2017-01-24 19:45:57 +0100

    dying_translator

---
+++ erlang/day3/selfstudy3.erl
@@ -0,0 +1,60 @@
+-module(selfstudy3).
+-export([setup_monitor/0]).
+-export([translate/1]).
+
+print(X) -> io:format("~p~n",[X]).
+
+
+dying_translate_service() ->
+    receive
+        {From, "casa"} ->
+            From ! "house",
+            dying_translate_service();
+        {From, "blanca"} ->
+            From ! "white",
+            dying_translate_service();
+        {From, _} ->
+            From ! "I don't understand.",
+            dying_translate_service();
+        diediedie ->
+            print("Translator dying!"),
+            exit(dead)
+    end.
+
+translate(Word) ->
+    translator ! {self(), Word},
+    receive
+        Translation -> Translation
+    end.
+
+translator_monitor() ->
+    process_flag(trap_exit, true),
+    receive
+        {'EXIT',_,_} ->
+            print("Dying translator caught."),
+            self() ! new,
+            translator_monitor();
+        new ->
+            print("Creating new translator."),
+            NewTranslator = spawn_link(fun dying_translate_service/0),
+            register(translator, NewTranslator),
+            translator_monitor();
+        exit ->
+            print("Monitor exiting") %,exit(yada)
+    end.
+
+setup_monitor() ->
+    Monitor = spawn(fun translator_monitor/0),
+    Monitor ! new,
+    Monitor.
+
+%Monitor = selfstudy3:setup_monitor().
+%selfstudy3:translate("casa").
+%translator ! diediedie.
+%monitor ! exit.
+%flush().
+%self() ! "fusk".
+
+
+
+
