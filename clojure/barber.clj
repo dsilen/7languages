2b67cdb3bcb01f528bf52e12fcf7d8f003eaa062
Daniel Silén <daniel@silen.se>
2017-02-06 04:52:27 +0100

    mitt i

--- clojure/barber.clj
+++ clojure/barber.clj
@@ -15,23 +15,69 @@
 
 (defn dostop [] (dosync (ref-set stop? true)))
 
-(def barber (agent nil))
 
 (def barberchair (ref nil))
 
-(def waitchairs (ref {:seats [] :free 3}))
+(def waitchairs (ref {:chairs [] :max 3}))
 
-; Customer dispatcher
+
+(defn incrementCounter [c]
+    (swap! c (fn [x] (+ x 1))))
+
+(defn sleepInterval
+    "Let the current thread sleep a random amount of msec between min and max (inclusive)."
+    [min max]
+    (let [interval (+ min (rand-int (+ 1 (- max min))))]
+        (Thread/sleep interval)
+        interval))
+
+; add-watch ska vi använda med barbern
+
+
+; Customer stuff
+(def customerCounter (atom 0))
+
+(defn getCustomerName []
+    (let [customerId (incrementCounter customerCounter)]
+        (str "Customer #" customerId)))
+
+
+; Customer dispatchern
 (def customer-dispatcher (agent nil))
 
-(defn do-dispatch [state]
+(defn addCustomerToWaitchairIfAvailable [chairs customer]
+    (do
+;    (println "adCust")
+    (let [
+        l (:chairs chairs)
+        c (:max chairs)]
+    (if (< (count l) c) ; den får nil ibland på c ?!
+        (assoc chairs :chairs (conj l customer))
+        chairs))))
+
+
+(defn do-dispatch [_]
     (loop []
         (if @stop?
             nil
             (do
-                (Thread/sleep 2000)
-                (println "hej")
-                (recur))
-        )))
+                (sleepInterval 1000 3000)
+;                (println "kör")
+                (dosync (alter waitchairs addCustomerToWaitchairIfAvailable (getCustomerName)))
+                (recur)))))
 
 (send customer-dispatcher do-dispatch)
+
+; barber
+(def barber (agent nil))
+
+(defn do-barb [_]
+    (if @stop?
+        nil
+        (do
+            (Thread/sleep 2000)
+            (println "barb")
+            (recur nil))))
+
+(send barber do-barb)
+
